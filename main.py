7938448c65ae6be8efe6896edd32d4f01f3e9948
makarchikof1 <vitaliymakarchikov@gmail.com>
2023-11-21 08:30:18 +0300

    1

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#pipimport pandas as pd
 import openpyxl
 import re
 
@@ -8,18 +7,8 @@ def excel_get():
     print(sheet[2][15].value)
     print(sheet.max_row)
     for i in range (3, sheet.max_row):
-        #print(sheet[i][15].value)
         s = re.findall(r'https.+,', sheet[i][15].value)
         print(s)
-
-
-
-    #excel_data_get = pd.read_excel('21718972__2022-01-01__2022-01-31.xlsx')
-    #print(excel_data_get)
-    #print(excel_data_get.columns.ravel())
-    #print(excel_data_get['Unnamed: 15'].)
-
-    #print(excel_data_get['Unnamed: 15'])
 
 def main():
     excel_get()
